04aa5ba0cd151d800038d425cc4b9faa5878a848
Iván Rodríguez Ruiz <ivanrguez1@yahoo.es>
2024-03-14 13:26:13 +0100

    Proyecto Inicial con Paquetes MVC

---
+++ src/main/java/com/soltel/islantilla/IslantillaApplication.java
@@ -0,0 +1,13 @@
+package com.soltel.islantilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslantillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslantillaApplication.class, args);
+	}
+
+}
