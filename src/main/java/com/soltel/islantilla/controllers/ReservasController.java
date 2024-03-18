3f73fa80db788cc9b6c3c0324d35346a966f9f6a
Iván Rodríguez Ruiz <ivanrguez1@yahoo.es>
2024-03-18 14:30:57 +0100

    [+] Clase 2024-03-18 Proyecto Islantilla Endpoints Clientes Reservas Parte2

--- src/main/java/com/soltel/islantilla/controllers/ReservasController.java
+++ src/main/java/com/soltel/islantilla/controllers/ReservasController.java
@@ -2,6 +2,7 @@ package com.soltel.islantilla.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soltel.islantilla.models.ClientesModel;
 import com.soltel.islantilla.models.ReservasModel;
 import com.soltel.islantilla.services.ClientesService;
 import com.soltel.islantilla.services.ReservasService;
@@ -13,12 +14,17 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -61,10 +67,75 @@ public class ReservasController {
         }
     }
     
+    // Método para insertar
+    // Endpoint de ejemplo: [POST] 
+    // http://localhost:8100/reservas/insertar/118/2024-04-06/12345678M/110.65/reserva_20240318_003.pdf/spa,masajes,balinesa
+    @PostMapping("/insertar/{hab}/{entrada}/{nif}/{precio}/{rutaPdf}/{opciones}")
+    public ResponseEntity<?> createReserva (@PathVariable int hab, @PathVariable LocalDate entrada,
+        @PathVariable String nif, @PathVariable float precio, 
+        @PathVariable String rutaPdf, @PathVariable String opciones) {
+        
+        // 1º Busco si la reserva YA existe
+        Optional<ReservasModel> reserva = reservasService.findReservaById(hab, entrada);
+        // 2º Tengo que buscar el cliente a partir del nif
+        Optional<ClientesModel> clienteBuscado = clientesService.findByClientesByNif(nif);
+
+        if(reserva.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Reserva existente!");
+        } else if(clienteBuscado.isPresent()){
+            ReservasModel nuevaReserva = new ReservasModel();
+            ClientesModel cliente = clienteBuscado.get();
+
+            // Hago la asignación de todos los atributos (incluido el objeto Cliente)
+            nuevaReserva.setHab(hab);
+            nuevaReserva.setEntrada(entrada);
+            nuevaReserva.setPrecio(precio);
+            nuevaReserva.setCliente(cliente);
+            nuevaReserva.setRutaPdf(rutaPdf);
+            nuevaReserva.setOpciones(opciones);
+
+            // Tengo que hacer la INSERCIÓN!!
+            ReservasModel reservaGuardada = reservasService.saveReserva(nuevaReserva);
+            return ResponseEntity.ok(reservaGuardada);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cliente no existe");
+        } 
+    }
     
     
-    
-    
+    // Método para actualizar
+    // NOTA: Ejecutar antes [POST] http://localhost:8100/clientes/insertar/23456789G/Sara/40/1
+    // Endpoint de ejemplo: [PUT] 
+    // http://localhost:8100/reservas/insertar/118/2024-04-06/23456789G/100.55/reserva_20240319_001.pdf/spa,masajes
+    @PutMapping("/insertar/{hab}/{entrada}/{nif}/{precio}/{rutaPdf}/{opciones}")
+    public ResponseEntity<?> updateReserva (@PathVariable int hab, @PathVariable LocalDate entrada,
+        @PathVariable String nif, @PathVariable float precio, 
+        @PathVariable String rutaPdf, @PathVariable String opciones) {
+        
+        // 1º Busco si la reserva YA existe
+        Optional<ReservasModel> reserva = reservasService.findReservaById(hab, entrada);
+        // 2º Tengo que buscar el cliente a partir del nif
+        Optional<ClientesModel> clienteBuscado = clientesService.findByClientesByNif(nif);
+
+        if(!reserva.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Reserva NO existe!");
+        } else if(clienteBuscado.isPresent()){
+            ReservasModel reservaActualizada = reserva.get();
+            ClientesModel cliente = clienteBuscado.get();
+
+            // Hago la asignación de todos los atributos (incluido el objeto Cliente)
+            reservaActualizada.setPrecio(precio);
+            reservaActualizada.setCliente(cliente);
+            reservaActualizada.setRutaPdf(rutaPdf);
+            reservaActualizada.setOpciones(opciones);
+
+            // Tengo que hacer la INSERCIÓN!!
+            ReservasModel reservaGuardada = reservasService.saveReserva(reservaActualizada);
+            return ResponseEntity.ok(reservaGuardada);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cliente no existe");
+        } 
+    }
     
     
     
